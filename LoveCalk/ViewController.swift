27c507704fe938868168f4b1c89cfd612a7e82d5
Karim <amri@mobiblanc.com>
2019-03-15 11:29:00 +0100

    first commit

---
+++ LoveCalk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoveCalk
+//
+//  Created by Mahdi Boukhris on 15/03/2019.
+//  Copyright © 2019 salaheddine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
